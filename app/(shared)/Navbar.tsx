be26fc3c7d15e459b2ab991a608f7a5a3d356c9d
Edward Didyk <eduard.didyk01@gmail.com>
2023-07-17 23:03:16 +0300

    Navbar in progress 1.3

--- app/(shared)/Navbar.tsx
+++ app/(shared)/Navbar.tsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const Navbar = (props: Props) => {
   return (
     <header className="mb-5">
       <nav className="flex justify-between items-center w-full bg-wh-900 text-wh-10 px-10 py-4">
-        navbar
+        <div className="hidden sm:block">social link</div>
+        <div className="flex justify-between items-center gap-10">
+          <Link href="/">Home</Link>
+          <Link href="/">Trending</Link>
+          <Link href="/">About</Link>
+        </div>
       </nav>
+      <div className="flex justify-between gap-8 mt-5 mb-4 mx-10">
+        <div className="basis-2/3 md:mt-3">
+          <h1 className="font-bold text-3xl md:text-5xl">BLOG OF THE FUTURE</h1>
+          <p className="text-sm mt-3">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+        </div>
+        <div className="basis-full relative w-auto bg-wh-500">
+          Image gonna be here.
+        </div>
+      </div>
+      <hr className="border-1 mx-10" />
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
