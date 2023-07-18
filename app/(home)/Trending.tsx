cfb4dfc4380eea33507ce1146eee079f2687c586
Edward Didyk <eduard.didyk01@gmail.com>
2023-07-18 22:03:10 +0300

    Trending comp in progress: img part option w/ flex

--- app/(home)/Trending.tsx
+++ app/(home)/Trending.tsx
@@ -1,17 +1,33 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const Trending = (props: Props) => {
   return (
     <section className="pt-3 pb-10">
       <div className="flex items-center gap-3">
-        <div className="bg-wh-900 py-2 px-8">
+        <div className="bg-wh-900 py-2 px-8 text-wh-10 text-sm font-bold">
           TRENDING
+        </div>
+        <p className="text-sm">
+          Cras rhoncus vitae ligula eget condimentum semper fermentum lacus ac
+          congue. Curabitu laoreet id arcu sed.
+        </p>
+      </div>
+      
+      {/* option w/ flex */}
+      <div className="flex justify-between gap-3 my-3">
+        <div className="basis-1/2 bg-wh-500 h-96"></div>
+        <div className="flex flex-col basis-1/2 gap-3 h-96">
+          <div className="basis-1/2 bg-wh-500"></div>
+          <div className="flex basis-1/2 gap-3">
+            <div className="basis-1/2 bg-wh-500"></div>
+            <div className="basis-1/2 bg-wh-500"></div>
+          </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Trending
+export default Trending;
