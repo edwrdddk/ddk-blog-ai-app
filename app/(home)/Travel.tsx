c72c82bf2860ed292f8850d95dcb4e0713e9029b
Edward Didyk <eduard.didyk01@gmail.com>
2023-07-22 01:31:31 +0300

    Travel comp in progress: header

--- app/(home)/Travel.tsx
+++ app/(home)/Travel.tsx
@@ -6,6 +6,13 @@ const Travel = (props: Props) => {
   return (
     <section className="mt-10">
       <hr className="broreder-1"/>
+      {/* HEADER */}
+      <div className="flex items-center gap-3 my-8">
+        <h4 className="bg-accent-green py-2 px-5 text-wh-900 text-sm font-bold">
+          TRAVEL
+        </h4>
+        <p className="font-bold text-2xl">Latest News in Tech</p>
+      </div>
     </section>
   )
 }
