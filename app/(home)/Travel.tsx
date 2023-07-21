604d645171657d77df653a43b0e576895eebba01
Edward Didyk <eduard.didyk01@gmail.com>
2023-07-22 01:28:59 +0300

    Travel comp in progress

---
+++ app/(home)/Travel.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Travel = (props: Props) => {
+  return (
+    <section className="mt-10">
+      <hr className="broreder-1"/>
+    </section>
+  )
+}
+
+export default Travel
