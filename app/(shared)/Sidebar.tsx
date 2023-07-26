d7e2a2dd7b6db35b5475c8026886f60719ffacbf
Edward Didyk <eduard.didyk01@gmail.com>
2023-07-26 23:03:34 +0300

    Sidebar comp in pr

--- app/(shared)/Sidebar.tsx
+++ app/(shared)/Sidebar.tsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React from "react";
+import SocialLinks from "./SocialLinks";
+import Subscribe from "./Subscribe";
 
-type Props = {}
+type Props = {};
 
 const Sidebar = (props: Props) => {
   return (
     <section>
-      <div className="flex items-center gap-3 my-8">
-        <h4 className="bg-accent-orange py-2 px-5 text-wh-900 text-sm font-bold">
-          HOT
-        </h4>
-        <p className="font-bold text-2xl">Latest News in Tech</p>
+      <h4 className="bg-wh-900 py-3 px-5 text-wh-50 text-xs font-bold text-center">
+        Subscribe and Follow
+      </h4>
+      <div className="my-5 mx-5">
+        <SocialLinks isDark />
       </div>
+      <Subscribe />
+      <div className="bg-wh-900 my-8">advert image</div>
+      <h4 className="bg-wh-900 py-3 px-5 text-wh-50 text-xs font-bold text-center">
+        About the blog
+      </h4>
+      <div className="bg-wh-900 my-8">profile image</div>
+      <h4 className="py-3 px-5 text-wh-500 font-bold text-center">
+        Eduard Didyk
+      </h4>
+      <p>
+        Consectetur semper vel ex quis accumsan. Pellentesque tincidunt justo id
+        neque semper, sed posuere leo vestibulum.
+      </p>
     </section>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
