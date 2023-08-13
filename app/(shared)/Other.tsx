7917cc9d4a5465d08ca465c7af8e238c7c3d56fc
Edward Didyk <eduard.didyk01@gmail.com>
2023-08-14 02:54:22 +0300

    travelPosts & otherPosts

--- app/(shared)/Other.tsx
+++ app/(shared)/Other.tsx
@@ -1,9 +1,12 @@
+import { Post } from "@prisma/client";
 import React from "react";
 import Card from "./Card";
 
-type Props = {};
+type Props = {
+  otherPosts: Array<Post>;
+};
 
-const Other = (props: Props) => {
+const Other = ({ otherPosts }: Props) => {
   return (
     <section className="pt-4 mb-16">
       <hr className="border-1" />
@@ -12,10 +15,26 @@ const Other = (props: Props) => {
 
       {/* CARDS GRID */}
       <div className="sm:grid grid-cols-2 gap-16">
-        <Card className="bg-wh-500 mt-5 sm:mt-0" imageHeight="h-80" />
-        <Card className="bg-wh-500 mt-5 sm:mt-0" imageHeight="h-80" />
-        <Card className="bg-wh-500 mt-5 sm:mt-0" imageHeight="h-80" />
-        <Card className="bg-wh-500 mt-5 sm:mt-0" imageHeight="h-80" />
+        <Card
+          className="bg-wh-500 mt-5 sm:mt-0"
+          imageHeight="h-80"
+          post={otherPosts[0]}
+        />
+        <Card
+          className="bg-wh-500 mt-5 sm:mt-0"
+          imageHeight="h-80"
+          post={otherPosts[1]}
+        />
+        <Card
+          className="bg-wh-500 mt-5 sm:mt-0"
+          imageHeight="h-80"
+          post={otherPosts[2]}
+        />
+        <Card
+          className="bg-wh-500 mt-5 sm:mt-0"
+          imageHeight="h-80"
+          post={otherPosts[3]}
+        />
       </div>
     </section>
   );
