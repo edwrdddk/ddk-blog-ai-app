63a52d188b50085f5559991cc3240f36436182e6
Edward Didyk <eduard.didyk01@gmail.com>
2023-08-25 04:20:36 +0300

    Content (client) component BREADCRUMBS

--- app/post/[id]/Content.tsx
+++ app/post/[id]/Content.tsx
@@ -1,12 +1,28 @@
 "use client";
-import React from 'react'
+import { FormattedPost } from "@/app/types";
+import React, { useState } from "react";
 
-type Props = {}
+type Props = {
+  post: FormattedPost;
+};
 
-const Content = (props: Props) => {
+const Content = ({ post }: Props) => {
+  const [isEditable, setIsEditable] = useState<boolean>(false);
+
+  const [title, setTitle] = useState<string>(post.title);
+  const [titleError, setTitleError] = useState<string>("");
+  // const [tempTitle, setTempTitle] = useState<string>(title);
+
+  const [content, setContent] = useState<string>(post.content);
+  const [contentError, setContentError] = useState<string>("");
+  // const [tempContent, setTempContent] = useState<string>(content);
+
   return (
-    <div>Content</div>
-  )
-}
+    <div className="prose w-full max-w-full mb-10">
+      {/* BREADCRUMBS */}
+      <h5 className="text-wh-300">{`Home > ${post.category} > ${post.title}`}</h5>
+    </div>
+  );
+};
 
-export default Content
+export default Content;
--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -17,7 +17,7 @@ const getPost = async (id: string) => {
   });
 
   if (!post) {
-    console.log(`Post with id ${id} not found`);
+    // console.log(`Post with id ${id} not found`);
     return null;
   }
 
@@ -39,7 +39,7 @@ const Post = async ({ params }: Props) => {
     <main className="px-10 leading-7">
       <div className="md:flex gap-10 mb-5">
         <div className="basis-3/4">
-          <Content />
+          <Content post={post} />
         </div>
         <div className="basis-1/4">
           <Sidebar />
